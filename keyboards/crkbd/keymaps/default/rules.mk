a3b33a810675d22ab31c34115e90586eba35a3af
Kyle Baumgarten <BaumDev@proton.me>
2025-06-27 02:29:14 -0400

    added my keymap

---
+++ keyboards/crkbd/keymaps/default/rules.mk
@@ -0,0 +1,13 @@
+#----------------------------------------------------------------------------
+# make gergo:germ:dfu
+# Make sure you have dfu-programmer installed!
+#----------------------------------------------------------------------------
+# Firmware options
+MOUSEKEY_ENABLE = yes  		# Mouse keys, needed for baller
+
+#Debug options
+VERBOSE 		 = no
+DEBUG_MATRIX_SCAN_RATE   = no
+DEBUG_BALLER 		 = no
+DEBUG_MATRIX		 = no
+
